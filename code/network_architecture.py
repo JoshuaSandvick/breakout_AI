32c426333e918c33ad1dbb6f39ea9becf9d964c2
josh.sandvick <josh.sandvick@gmail.com>
2017-11-09 17:03:22 -0500

    Fixed up the network architecture.

--- code/network_architecture.py
+++ code/network_architecture.py
@@ -10,22 +10,28 @@ def create_network(batch_size):
 
     target = tf.placeholder(shape=[None], dtype=tf.float32)
     
-    
     # Input layer
     input_layer = tf.placeholder(tf.float16, shape=(batch_size, 84, 84, num_frames_back_to_convolve))
     
     # First hidden layer
-    hidden_layer_1 = tf.nn.convolution(input_layer, [8, 8, num_frames_back_to_convolve, num_hidden_layer_1_output_channels], padding="VALID", strides=[4, 4])
+    filter_1 = tf.Variable(tf.random_normal([8, 8, num_frames_back_to_convolve, num_hidden_layer_1_output_channels], dtype=tf.float16), dtype=tf.float16)
+    hidden_layer_1 = tf.nn.conv2d(input_layer, filter_1, padding="VALID", strides=[1, 4, 4, 1])
     hidden_layer_1 = tf.nn.relu(hidden_layer_1)
+    hidden_layer_1 = tf.nn.max_pool(hidden_layer_1, [1, 2, 2, 1], [1, 2, 2, 1], padding="VALID")
     
     # Second hidden layer
-    hidden_layer_2 = tf.nn.convolution(hidden_layer_1, [4, 4, num_hidden_layer_1_output_channels, num_hidden_layer_2_output_channels], padding="VALID", stries=[2, 2])
+    filter_2 = tf.Variable(tf.random_normal([4, 4, num_hidden_layer_1_output_channels, num_hidden_layer_2_output_channels], dtype=tf.float16), dtype=tf.float16)
+    hidden_layer_2 = tf.nn.conv2d(hidden_layer_1, filter_2, padding="VALID", strides=[1, 2, 2, 1])
     hidden_layer_2 = tf.nn.relu(hidden_layer_2)
+    hidden_layer_2 = tf.nn.max_pool(hidden_layer_2, [1, 2, 2, 1], [1, 2, 2, 1], padding="VALID")
+    
+    # Flatten the hidden layer's output for the fully connected layer
+    flattened = tf.reshape(hidden_layer_2, [batch_size, -1])
     
     # First fully-connected layer
-    fc_layer_1 = tf.layer.dense(hidden_layer_2, units=num_fc_1_output_units, activation=tf.nn.relu)
+    fc_layer_1 = tf.layers.dense(flattened, units=num_fc_1_output_units, activation=tf.nn.relu)
     
     # Output layer
-    output_layer = tf.layer.dense(fc_layer_1, units=num_output_actions)
+    output_layer = tf.layers.dense(fc_layer_1, units=num_output_actions)
     
     return output_layer
